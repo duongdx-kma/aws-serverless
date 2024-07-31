62ecf2a53da2f18aa74f83ea71657c1051a41f0f
duongdx-kma <duong.dinh.xuan998@gmail.com>
2024-07-31 09:21:35 +0700

    aws serverless notes

--- 3.api-gateway-body-mapping-template/lambda_function.py
+++ 3.api-gateway-body-mapping-template/lambda_function.py
@@ -2,9 +2,10 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    print(event)
 
-    input_1 =  event['input_1']
-    input_2 =  event['input_2']
+    input_1 = int(event['input']['input_1'])
+    input_2 = int(event['input']['input_2'])
 
     match event['operation']:
         case 'add':
@@ -15,8 +16,26 @@ def lambda_handler(event, context):
             result = input_1 * input_2
         case 'divide':
             result = input_1 / input_2
+        case _:
+            print("The language doesn't matter, what matters is solving problems.")
+            result = 0
 
     return {
         'statusCode': 200,
         'body': json.dumps('result is {}'.format(result))
     }
+
+'''
+# comment
+
+# document:
+https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+
+# Testing:
+curl --location 'https://domain/dev/math/add' \
+--header 'Content-Type: application/json' \
+--data '{
+    "input_1": 5,
+    "input_2": 10
+}'
+'''
